c6172c2ea20e5a12133228106841612c0b112a2b
Dominika Bienkowska <bienkowska.dominika@gmail.com>
2015-03-01 20:54:07 +0100

    first navigation and open button

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -9,15 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorSquare: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateBackgroundColor()
+        colorSquare.layer.borderColor = UIColor.blackColor().CGColor
+        colorSquare.layer.borderWidth = 1
     }
 //is it tracked?
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func updateBackgroundColor() {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
 
 
