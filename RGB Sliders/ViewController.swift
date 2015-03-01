e9e65e69f7e6271e7210838d6192ad876c34cc69
Dominika Bienkowska <bienkowska.dominika@gmail.com>
2015-03-01 15:50:41 +0100

    comment

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+//is it tracked?
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
